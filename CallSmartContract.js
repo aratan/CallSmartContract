477b494262aece4e19edc55f9e0705e1c5d1bf0c
Victor Arbiol <victor.arbiol@gmail.com>
2024-05-01 15:33:52 +0200

    Update CallSmartContract.js

--- CallSmartContract.js
+++ CallSmartContract.js
@@ -1,26 +1,23 @@
+function smartcontract(contractAddress, contractABI, providerKey, functionName, outputElementId) {
+    var web3 = new Web3(new Web3.providers.HttpProvider(providerKey));
 
-function smartcontract(contractAddress, key){
-        var web3 = new Web3(new Web3.providers.HttpProvider('https://polygon-amoy.infura.io/v3/'+key));
-        var contractABI = [{ "inputs": [{ "internalType": "string", "name": "_initialName", "type": "string" }], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "getName", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }];
-        
-        // Crear instancia del contrato
-        var contractInstance = new web3.eth.Contract(contractABI, contractAddress);
+    // Crear instancia del contrato
+    var contractInstance = new web3.eth.Contract(contractABI, contractAddress);
 
-        // Ejemplo de función para obtener el nombre del contrato
-        function obtenerNombreContrato() {
-            contractInstance.methods.getName().call((error, result) => {
-                if (!error) {
-                    console.log('Nombre del contrato:', result);
-                    document.getElementById("ROOT").innerHTML = `<h1> ${result} </h1>`;
-                } else {
-                    console.error('Error al obtener el nombre:', error);
-                    document.getElementById("ROOT").innerHTML = `<h1> ${error} </h1>`;
-
-                }
-            });
-        }
-
-        // Llamar a la función para obtener el nombre del contrato
-        obtenerNombreContrato();
-        console.log(obtenerNombreContrato());
+    // Función para llamar a cualquier método del contrato
+    function callContractMethod() {
+        contractInstance.methods[functionName]().call((error, result) => {
+            if (!error) {
+                console.log('Resultado:', result);
+                document.getElementById(outputElementId).innerHTML = `${result}`;
+            } else {
+                console.error('Error al llamar al método:', error);
+                document.getElementById(outputElementId).innerHTML = `${error}`;
+            }
+        });
     }
+
+    // Llamar a la función
+    callContractMethod();
+    console.log(callContractMethod());
+}
